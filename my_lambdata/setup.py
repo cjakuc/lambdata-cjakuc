fbb989e92037eb6aab3bee39903f3874e36311f1
Chris Jakuc <chris.jakuc@gmail.com>
2020-03-09 14:25:54 -0400

    Added setup and gitignore

---
+++ my_lambdata/setup.py
@@ -0,0 +1,19 @@
+
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="cjakuc-lambdata-cjakuc",
+    version="1.0",
+    author="Chris Jakuc",
+    author_email="chris.jakuc@gmail.com",
+    description="Learning how to create a package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cjakuc/lambdata-cjakuc",
+    packages=find_packages()
+
+)
