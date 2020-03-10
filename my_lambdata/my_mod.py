7ec596194fedf473aece34b3bc7d7277711c59fa
Chris Jakuc <chris.jakuc@gmail.com>
2020-03-10 19:33:04 -0400

    Created a Df_fancy class with 2 methods

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -4,10 +4,70 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from scipy import stats
+import numpy as np
+import random
+
+class Df_fancy():
+
+    def __init__(self, df):
+        self.df = df
+        
+    
+    def set_train_val_test(self, random_state = 42):
+        """
+        Returns train, validation, and test sets
+        """
+        train, test = train_test_split(self.df, random_state = random_state)
+        train, val = train_test_split(train, random_state = random_state)
+        return train, val, test
+    
+    def generate_data(self, rows_to_add, random_state=42):
+        """
+        Param rows_to_add is an int that specifies the new number of rows to add
+        """
+        rows = []
+        for i in range(0, len(self.df.columns)-1):
+            rows.append([])
+        for row in range(0,rows_to_add):
+            for col in self.df.columns.tolist():
+                rows[row].append(random.choices(np.unique(self.df[col]).tolist()))
+        temp_df = pd.DataFrame(rows, columns=['target','cat1','cat2'])
+        print(self.df.head())
+        print(temp_df.head())
+        self.df = pd.concat([self.df, temp_df])
+        return self.df
+        
+
+    # def set_chi_2_report(self, cat1, cat2):
+    #     """
+    #     Param cat1 is a categorical variable
+    #     Param cat2 is another categorical variable
+    #     Returns a contingency table and a chi squared test
+    #     """
+    #     self.contingency_table = pd.crosstab(self.df[cat1], self.df[cat2])
+    #     self.chi2_test = stats.chi2_contingency(contingency_table)
+    #     return self.contingency_table, self.chi2_test
+
+    # def get_contingency(self):
+    #     """
+    #     Returns contingency table for cat 1 and 2
+    #     """
+    #     return self.contingency_table
+
+    # def get_chi2(self):
+    #     """
+    #     Returns chi2 table for cat 1 and 2
+    #     """
+    #     return self.chi2
+
+    # def get_chi2_report(self):
+    #     """
+    #     Returns contingency table and chi2 table for at 1 and cat 2
+    #     """
+    #     return self.contingency_table, self.chi2
+
 
 # An example function
-
-
 def enlarge(n):
     """
     Param n is a number
@@ -16,23 +76,23 @@ def enlarge(n):
     return n * 100
 
 
-def train_val_test_split(df, random_state=42):
-    """
-    Param df is a dataframe
-    Function will retun a train, validate, test split with default sizes
-    """
-    train, test = train_test_split(df)
-    train, val = train_test_split(train)
+# def train_val_test_split(df, random_state=42):
+#     """
+#     Param df is a dataframe
+#     Function will retun a train, validate, test split with default sizes
+#     """
+#     train, test = train_test_split(df)
+#     train, val = train_test_split(train)
 
-    return train, val, test
+#     return train, val, test
 
 
-def chi_2_report(df, a, b):
-    """
-    Param df is a dataframe
-    Param a is a categorical variable
-    Param b is a categorical variable
-    Function will return a contingnecy table and a chi squared test
-    """
-    contingency_table = pd.crosstab(df[a], df[b])
-    return contingency_table, stats.chi2_contingency(contingency_table)
+# def chi_2_report(df, a, b):
+#     """
+#     Param df is a dataframe
+#     Param a is a categorical variable
+#     Param b is a categorical variable
+#     Function will return a contingnecy table and a chi squared test
+#     """
+#     contingency_table = pd.crosstab(df[a], df[b])
+#     return contingency_table, stats.chi2_contingency(contingency_table)
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,12 +1,39 @@
 
 # mylambdata/my_script.py
 
-from my_lambdata.my_mod import enlarge
+from my_lambdata.my_mod import enlarge, Df_fancy
 import pandas as pd
 
-print("Hello World")
+# print("Hello World")
 
 
-x = 5
-print("Number", x)
-print("Enlarged number", enlarge(x))
+# x = 5
+# print("Number", x)
+# print("Enlarged number", enlarge(x))
+
+# Test the generate_data function
+df = pd.DataFrame(data={'target':[0,1,1,1,0,0,0,1,0,1],
+                        'cat1':['a','b','c','d','e','f','g','h','i','j'],
+                        'cat2':['1','2','3','4','5','6','7','8','9','10']})
+
+# Instantiate an instance of Df_fancy
+df_fancy = Df_fancy(df)
+
+# See shape before generating new data
+print(df_fancy.df.shape)
+
+# Generate new data for 2 rows
+df_fancy.generate_data(rows_to_add=2)
+
+# Check the shape to see if it changed correctly
+print(df_fancy.df.shape)
+
+# list1 = []
+# for i in range(0, len(df.columns)):
+#     list1.append([])
+# list1 = [[],[]]
+# list1[0].append(0)
+# list1[0].append(1)
+# list1[1].append(2)
+# list1[1].append(3)
+# print(list1)
